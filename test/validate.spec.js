e1ef6134866aabaf18f6e0197bce8a153b6752ae
busyhe <busyhe@qq.com>
2022-06-26 20:29:05 +0800

    fix: semi is never

--- test/validate.spec.js
+++ test/validate.spec.js
@@ -8,14 +8,14 @@ const { ESLint } = require('eslint')
 
 test('load config in eslint to validate all rule syntax is correct', async () => {
     const eslint = new ESLint()
-    const code = 'const foo = 1\nconst bar = function() {}\nbar(foo)\n'
+    const code = 'const foo = 1;\nconst bar = function() {};\nbar(foo);\n'
     const [lintResult] = await eslint.lintText(code)
     expect(lintResult.errorCount).toEqual(0)
 })
 
 test('ensure we allow top level await', async () => {
     const eslint = new ESLint()
-    const code = 'const foo = await 1\nconst bar = function() {}\nawait bar(foo)\n'
+    const code = 'const foo = await 1;\nconst bar = function() {};\nawait bar(foo);\n'
     const [lintResult] = await eslint.lintText(code)
     expect(lintResult.errorCount).toEqual(0)
 })
